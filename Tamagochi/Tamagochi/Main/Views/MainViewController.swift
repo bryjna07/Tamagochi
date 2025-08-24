ac51f9176c68a659197c9336e9c83a039727ea45
Youngjin <bryjna07@nate.com>
2025-08-24 17:02:43 +0900

    Feat: 초기세팅, 폴더생성 및 라이브러리 추가

---
+++ Tamagochi/Tamagochi/Main/Views/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  Tamagochi
+//
+//  Created by YoungJin on 8/24/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
